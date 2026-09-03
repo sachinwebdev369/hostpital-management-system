e366b1ac6863e42e8a6675b7a56968dc0a8ec3e2
sachinwebdev369 <sachinwebmern@gmail.com>
2026-09-03 10:52:05 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const colors = require("colors")
+const morgan = require("morgan")
+
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+// dotenv
+require("dotenv").config();
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'))
 
-// Home route
+
+// route
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -16,80 +24,9 @@ app.get("/", (req, res) => {
   });
 });
 
-// Get all products
-app.get("/api/products", (req, res) => {
-  const products = [
-    {
-      id: 1,
-      name: "T-Shirt",
-      price: 599,
-    },
-    {
-      id: 2,
-      name: "Shoes",
-      price: 1499,
-    },
-  ];
-
-  res.json({
-    success: true,
-    products,
-  });
-});
-
-// Get single product
-app.get("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-
-  res.json({
-    success: true,
-    message: `Product ID is ${productId}`,
-  });
-});
-
-// Create product
-app.post("/api/products", (req, res) => {
-  const { name, price } = req.body;
-
-  res.status(201).json({
-    success: true,
-    message: "Product created successfully",
-    product: {
-      name,
-      price,
-    },
-  });
-});
-
-// Update product
-app.put("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-  const { name, price } = req.body;
-
-  res.json({
-    success: true,
-    message: "Product updated successfully",
-    product: {
-      id: productId,
-      name,
-      price,
-    },
-  });
-});
-
-// Delete product
-app.delete("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-
-  res.json({
-    success: true,
-    message: `Product ${productId} deleted successfully`,
-  });
-});
 
 // Server
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`.bgCyan.white);
 });
